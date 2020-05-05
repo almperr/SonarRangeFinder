6e9003ec1fffd77916598a76e8caf8182601a320
Alex Perr <40256689+almperr@users.noreply.github.com>
2020-05-05 11:44:59 -0400

    Update functions.c

--- ECET309_Lab10&11/Sources/functions.c
+++ ECET309_Lab10&11/Sources/functions.c
@@ -2,7 +2,7 @@
 //Class:ECET 309
 //Assignment:Lab 10 & 11 - Sonar  
 //Date:3/29/2018
-//File Description: This file is for most of the functions used
+//File Description: This file contains functions that drive the sonar module to send a pulse, used ECT to time echo, and display data on LCD  
 
 #include <hidef.h>      /* common defines and macros */
 #include <stdio.h>
@@ -19,7 +19,7 @@ void SonarStart(void)           //This function clears flags to 6 and 7 capture 
 
 void SonarCheck(void)           //This function takes data from the sonar and calculates the distance and send the string to the LCD 
 {
-   
+   //variables
    volatile unsigned int init;
    volatile unsigned int echo;
    volatile unsigned int travelTime;
@@ -37,24 +37,24 @@ void SonarCheck(void)           //This function takes data from the sonar and ca
       ;
    }
                                          
-   echo = ECT_TC7;                       //stores the value from caputure timer 7 and associates it with the echo return time. 
+   echo = ECT_TC7;                       //stores the value from caputure timer 7 and associates it with the echo return time 
    
    if(init > echo)                                   //checks to see if initial value is greater than the return value 
    {
-      travelTime = ((echo + (65535 - init))/2);      //if it is greater, then it send the total travel time to a variable called travel time. 
+      travelTime = ((echo + (65535 - init))/2);      //if it is greater, then it sends the total travel time to a variable called travel time
    }
    else 
    {
       travelTime = ((echo + init)/2);                //if echo is not less than the initial value, then travel time becomes the sum of echo and the initial time. It gets divided by two in order to compensate for the time going there and back. 
    }
    
-   d = (1116*travelTime)/prescale;                   //this converts the distance from the value stored on the computer into feet and inches. 
+   dist = (1116*travelTime)/prescale;                   //this converts the time from the value stored in memory from ECT, into feet and inches
    
-   feet = d;
-   inches = d%12;
+   feet = dist;
+   inches = dist%12;
     
    
-   sprintf(buffer, "D=%d Feet %d Inches", feet,inches);       //This sends the feet and inches value into the buffer array in the appropriate locations in the string. 
+   sprintf(buffer, "D=%d Feet %d Inches", feet,inches);       //This sends the feet and inches value into the buffer array in the appropriate locations in the string 
    
    DisplayString(buffer);                                     //Sends the buffer array string the the LCD
         
